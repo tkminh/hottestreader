b3112e6493abd54a0fa8f2b88c679bb5de66ee58
minht <truongkimminh@gmail.com>
2013-07-20 00:33:22 +0700

    update

--- src/com/hotteststudio/constant/Default.java
+++ src/com/hotteststudio/constant/Default.java
@@ -6,4 +6,10 @@ public class Default {
 	public static final String URL_PDF_2_EPUB = "http://www.2epub.com/";
 	public static final int WIDTH = 800;
 	public static final int HEIGHT = 1280;
+	
+	public static final String URL_MANYBOOK = "http://manybooks.net";
+	public static final String URL_SNEE= "http://www.snee.com/epubkidsbooks";
+	public static final String URL_MOBILEREAD = "http://www.mobileread.com/forums/forumdisplay.php?f=130";
+	public static final String URL_FEEDBOOK = "http://www.feedbooks.com/original";
+	public static final String URL_VIETNAMBOOK = "http://motsach.info/";
 }
--- src/com/hotteststudio/epubreader/IntroActivity.java
+++ src/com/hotteststudio/epubreader/IntroActivity.java
@@ -1,5 +1,8 @@
 package com.hotteststudio.epubreader;
 
+import com.hotteststudio.constant.Default;
+import com.hotteststudio.util.XCommon;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -25,5 +28,33 @@ public class IntroActivity extends Activity {
 			finish();
 		}
 		super.onBackPressed();
+	}
+	
+	public void clickHowTo(View v) {
+		//XCommon.openBrowser(this, Default.URL_BOOK);
+	}
+	
+	public void clickConvert(View v) {
+		XCommon.openBrowser(this, Default.URL_PDF_2_EPUB);
+	}
+	
+	public void clickManyBook(View v) {
+		XCommon.openBrowser(this, Default.URL_MANYBOOK);
+	}
+	
+	public void clickSnee(View v) {
+		XCommon.openBrowser(this, Default.URL_SNEE);
+	}
+	
+	public void clickMobileRead(View v) {
+		XCommon.openBrowser(this, Default.URL_MOBILEREAD);
+	}
+	
+	public void clickFeedBook(View v) {
+		XCommon.openBrowser(this, Default.URL_FEEDBOOK);
+	}
+	
+	public void clickVNBook(View v) {
+		XCommon.openBrowser(this, Default.URL_VIETNAMBOOK);
 	}
 }
